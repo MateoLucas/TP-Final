4c7ae18c362a96857a265aa7f4faf0b859b3fc38
MateoLucas <mlucas@itba.edu.ar>
2021-03-13 15:26:22 -0300

    commit 6

--- TP-Final/main.h
+++ TP-Final/main.h
@@ -13,22 +13,25 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#ifdef MAIN_ALLEGRO_H
+//#ifdef MAIN_ALLEGRO_H
 #define SCREEN_WIDTH 200
 #define SCREEN_HEIGHT 200
 #define SHOT_SPEED 5
 #define ALIENS_N 55
+#define MURO_N 48
 
 #endif /* MAIN_ALLEGRO_H */
-
+/*
 #ifdef MAIN_RASP_H
 
 #define SCREEN_WIDTH 16
 #define SCREEN_HEIGHT 16
 #define SHOT_SPEED 1
 #define ALIENS_N 10
+#define MURO_N
 
 
 #endif
         
 #endif
+*/
