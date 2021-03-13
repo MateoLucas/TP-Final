b97167db12bcb927e6d0c5db8ec9ac87c2f143c4
MateoLucas <mlucas@itba.edu.ar>
2021-03-13 15:01:38 -0300

    commit 5

---
+++ TP-Final/muros.c
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+#include <stdbool.h>
+typedef struct MURO
+{
+    int x, y;
+    int life;
+    bool used;
+} MURO;
+
+
+
+
