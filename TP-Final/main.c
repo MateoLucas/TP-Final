0c6c5b60d29ab4a84fd32d4200bcac2bd4b90ec0
MateoLucas <mlucas@itba.edu.ar>
2021-03-13 11:39:13 -0300

    commit 2

--- TP-Final/main.c
+++ TP-Final/main.c
@@ -13,12 +13,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_image.h>
 
-/*
- * 
- */
 int main(int argc, char** argv) {
 
+    
+    
+    
+    
+    
+    
+    
+    
     return (EXIT_SUCCESS);
 }
 
