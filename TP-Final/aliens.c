70f9b126dc5f84b5fad526758ad224d0e55fa4c2
mariano <mariano@mariano-linux>
2021-03-13 15:25:07 -0300

    aliens

--- TP-Final/aliens.c
+++ TP-Final/aliens.c
@@ -7,21 +7,60 @@
 #include <stdbool.h>
 #define ALIEN_POS_REFERENCE_X
 #define ALIEN_POS_REFERENCE_Y
+#define ALIEN_V 2
 //la cantidad de aliens es siempre la misma, sin importar nivel
 typedef struct {
     int x;
     int y;
     int lives;
     bool alive;
-    bool shot;//shot is active
+    bool shot;
 
 }ALIEN;
 
 
-ALIEN alien_init(ALIEN alien, int i)
+void aliens_init(ALIEN* aliens,int life)
 {
-    alien.x = ALIEN_POS_REFERENCE_X + i;
-    alien.y = ALIEN_POS_REFERENCE_Y
+
+    int t;
+    int columnas = 0;
+    for(t=0; t < ALIENS_N; t++)
+    {
+        int s = t/11;
+        columnas++;
+        aliens[t].alive = true;
+        if (t > ALIENS_N-11) {
+            aliens[t].shot = true;
+        }
+        else {
+            aliens[t].shot = false;
+        }
+        aliens[t].lives = life;
+        if(columnas >= N_COLS)
+        {
+            columnas=0;
+        }
+        aliens[t].x = REFERENCE_X + columnas*ALIEN_SPACE_BUFFER;
+        aliens[t].y = REFERENCE_Y+ s* ALIEN_SPACE_BUFFER;
+    }
 }
 
-
+void aliens_update (ALIEN* aliens) {
+    int j;
+    int i;
+    int direccion = 1;
+    for (j=0;j<ALIENS_N;J++) {
+        aliens.x += direccion*ALIEN_V;
+        if ((aliens[j].alive)&&((aliens[j].x >= BUFFER_W-ALIEN_W)||aliens[j].x<= 0)) {
+            if (direccion)
+                direccion=1;
+            else
+                direccion=-1;
+            for (i=0;i<ALIENS_N;i++) {
+                aliens.x += direccion*ALIEN_V;
+                
+            }
+        }
+        
+    }
+}
